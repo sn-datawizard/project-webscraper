b3aa5a401d7fefd1b6d76fcd871b1ef7e0142b48
sn-datawizard <77932366+sn-datawizard@users.noreply.github.com>
2022-09-25 14:37:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import pymongo as pym
-import pyodbc
-import sqlalchemy
 
 def request(x):
 	url = x
@@ -15,7 +13,7 @@ def request(x):
 
 
 def upload(x,y):
-	client = pym.MongoClient("mongodb+srv://admin:superadmin@cluster0llc.chzx6.mongodb.net/database?retryWrites=true&w=majority")
+	client = pym.MongoClient("mongodb+srv://<user>:<password>@<cluster>/database?retryWrites=true&w=majority")
 	db = client["database"]
 	collection = db[f"{x}"]
 	dictonary = pd.DataFrame.to_dict(y, orient="records")
@@ -26,21 +24,3 @@ def upload(x,y):
 def merge(x, y):
 	merged = pd.merge(x, y, how="left", on="Country")
 	return merged
-
-"""
-def export(x):
-	client = pym.MongoClient("mongodb+srv://admin:superadmin@cluster0llc.chzx6.mongodb.net/database?retryWrites=true&w=majority")
-	db = client["database"]
-	collection = db[f"{x}"]
-	dataselect = collection.find({})
-	return pd.DataFrame.from_dict(dataselect).drop("_id", axis=1)
-"""
-
-"""
-def importsql(x):
-	table = x
-	engine = sqlalchemy.create_engine('mssql+pyodbc://sa:superadmin@DESKTOP-FRFT9OU/lcc?driver=SQL+Server+Native+Client+11.0')
-	#engine = sqlalchemy.create_engine('mssql+pyodbc://sa:superadmin@172.28.128.1,1433/lcc?driver=ODBC+Driver+18+for+SQL+Server')
-	df = export(x)
-	return df.to_sql(table, engine, if_exists="replace", index=False)
-"""
